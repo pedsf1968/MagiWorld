a8da562c543d293ae66f0ad4fd35fc3242bce32e
PEDSF1968 <pe.dsf@free.fr>
2018-10-31 16:41:38 +0100

    Add Main and MagiWorld.iml to git

---
+++ src/com/dsf/pe/Main.java
@@ -0,0 +1,16 @@
+package com.dsf.pe;
+public class Main {
+
+    private static int NOMBRE_JOUEURS = 2; //define 2 characters
+
+    /**
+     * Main
+     * @param args
+     */
+    public static void main(String[] args) {
+        Jeu jeu = new Jeu();
+
+        jeu.initialise(NOMBRE_JOUEURS);
+        jeu.execute();
+    }
+}
